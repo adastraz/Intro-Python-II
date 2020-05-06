83906df1f37ff5004b7e195069c1ec4a78dc4a61
adastraz <tylergosox@gmail.com>
2020-05-06 18:41:24 -0400

    going to write out some basics in notes
    wanted to get in and play around with how i will go about doing this

--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+    def __str__(self):
+        return f'Characters name : {self.name}'
+
+while True: 
+    username = input('Please choose your characters name : ')
+    if len(username) < 20:
+        new_player = Player(username, 'one')
+    else:
+        print('your name is too long, under 20 characters')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, items, description, n_to, s_to, e_to, w_to):
+        self.name = name
+        self.items = items
+        self.description = description
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+    def __str__(self):
+        return f'You are in the room : {self.name} \nThere are these items from within : {self.items} \nDescription : {self.description}'
